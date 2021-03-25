f93b1cab42e85c744651c6ca75a03ee4266607e6
[Urszula Sołtykowska] <[urssol@st.amu.edu.pl]>
2021-03-25 13:29:28 +0100

    bez 5

--- Soltykowska.R
+++ Soltykowska.R
@@ -36,4 +36,15 @@ horoskop("Ula", 1)
 
 #zad4
 
-pomiary = "2019-03-11: 23.5, 19/03/12: 12.7, 2019.03.13: 11.1, 2019-marzec-14: 14
+pomiary = "2019-03-11: 23.5, 19/03/12: 12.7, 2019.03.13: 11.1, 2019-marzec-14: 14.3"
+
+pomiary = str_replace_all(pomiary, pattern = "[\\/|.]", replacement = "\\-")
+
+str_extract_all(pomiary, pattern = "[0-9]+[\\-][a-z0-9]+[\\-][0-9]*")
+
+daty = str_extract_all(pomiary, pattern = "[0-9]+[\\-|/|.][a-z0-9]+[\\-|/|.][0-9]+")
+daty
+
+#zad5
+
+
